e7525e850a9b7f471cae1b4e0f6709639e5b20ed
Graham Campbell <graham@cachethq.io>
2015-05-23 15:03:10 +0100

    Updated my email

--- src/ConnectorInterface.php
+++ src/ConnectorInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Manager.
  *
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,7 @@ namespace GrahamCampbell\Manager;
 /**
  * This is the connector interface.
  *
- * @author Graham Campbell <graham@mineuk.com>
+ * @author Graham Campbell <graham@cachethq.io>
  */
 interface ConnectorInterface
 {
